67fb3081bf29461ae460923b93950bb2b20dd32f
Tyler Gerber <tcgerber10567@gmail.com>
2015-03-08 18:28:16 -0400

    fountain.js mostly finished.

--- js/fountain.js
+++ js/fountain.js
@@ -3,14 +3,32 @@
 var app = app || {};
 
 app.Fountain = function(){
-	function Fountain(x,y,width,height){
+	function Fountain(x,y,width,height,canvasWidth,canvasHeight,image){
 		this.x=x;
 		this.y=y;
 		this.width = width;
 		this.height = height;
 		this.droplets = [];
 		this.rate = 5;
+		this.canvasHeight = canvasHeight;
+		this.canvasWidth - canvasWidth;
+		this.image = image;
 	};
 	
 	var f = Fountain.prototype;
+	
+	f.update = function(dt,rate){
+		this.rate = rate;
+		for(int i = 0; i < rate*dt; i++){
+			this.droplets.push(new app.waterDroplet(this.x,this.y,app.utils.getRandom(.1,5),app.utils.getRandom(3,9),this.canvasWidth,this.canvasHeight));
+		}
+	};
+	
+	f.draw = function(ctx){
+		if(!this.image){
+			
+		}else{
+			ctx.drawImage(this.image,this.x,this.y);
+		}
+	};
 }();
